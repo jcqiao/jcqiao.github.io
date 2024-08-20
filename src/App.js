3bd26a0ec7b34a98566b3c23296e0282ce688efa
jcqiao <chunqiao21@gmail.com>
2024-08-19 23:32:14 -0700

    Add my first profile page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import Profile from "./Components/Profile";
 
 function App() {
-  return <Profile />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/profile" replace />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,70 +1,141 @@
 import React from "react";
-import { Container, Typography, Avatar, Grid, Box } from "@mui/material";
+import {
+  Container,
+  Typography,
+  Avatar,
+  Grid,
+  Box,
+  Divider,
+  IconButton
+} from "@mui/material";
+import avatar from "../assets/avatar.png";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Profile = () => {
+  const goto = () => {
+    window.location.href = "https://github.com/jcqiao";
+  };
   return (
     <Box className="profile-card">
-      <Container maxWidth="md" sx={{ width: "80% ", height: "80% " }}>
-        <Box textAlign="center" mt={5}>
-          <Avatar
-            alt="Your Name"
-            src="/path-to-your-image.jpg" // 替换为你的头像路径
-            sx={{ width: 120, height: 120, margin: "auto" }}
-          />
-          <Typography variant="h4" component="h1" gutterBottom>
-            你的名字
-          </Typography>
-          <Typography variant="h6" color="textSecondary" paragraph>
-            前端开发工程师 | 无障碍性设计专家
-          </Typography>
-        </Box>
+      <Box
+        sx={{
+          position: "relative",
+          padding: "20px",
+          borderRadius: "8px",
+          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // 柔和的阴影
+          border: "1px solid #ddd",
+          overflow: "hidden",
+          height: "100%"
+        }}
+      >
+        <Box
+          sx={{
+            content: '""',
+            position: "absolute",
+            width: "0px",
+            height: "0px",
+            borderStyle: "solid",
+            borderWidth: "0 0 60px 60px",
+            borderColor: "transparent transparent #f5f5dc transparent",
+            top: 0,
+            right: 0,
+            zIndex: 1,
+            transform: "rotate(90deg)"
+          }}
+        />
+        <Container maxWidth="md" sx={{ height: "100%" }}>
+          <Grid
+            container
+            spacing={2}
+            sx={{ height: "100%" }}
+            alignContent="center"
+          >
+            {/* 左侧部分 */}
+            <Grid item xs={12} sm={6}>
+              <Box textAlign="center" mt={5}>
+                <Avatar
+                  alt="Chunqiao"
+                  src={avatar}
+                  sx={{ width: 120, height: 120, margin: "auto" }}
+                />
+                <Typography variant="h4" component="h1" gutterBottom>
+                  Chunqiao Jiang
+                </Typography>
+                <Typography variant="h6" color="textSecondary" paragraph>
+                  Front-End Developer
+                </Typography>
+                <Typography variant="h6" color="textSecondary" paragraph>
+                  Blender & Game Developer
+                </Typography>
+                <IconButton aria-label="visit my github" onClick={goto}>
+                  <GitHubIcon sx={{ position: "absolute" }} />
+                </IconButton>
+              </Box>
+            </Grid>
 
-        <Box mt={4}>
-          <Typography variant="h5" component="h2" gutterBottom>
-            关于我
-          </Typography>
-          <Typography variant="body1" paragraph>
-            你好，我是一名充满激情的前端开发工程师，专注于创建用户友好和高效的Web应用程序。
-            我拥有丰富的开发经验，尤其擅长无障碍性设计，并且熟练使用 Material-UI 等前端工具库。
-          </Typography>
+            {/* 中间虚线分隔 */}
+            <Divider
+              orientation="vertical"
+              flexItem
+              sx={{ borderStyle: "dashed" }}
+            />
+            <Grid item xs={12} sm={5}>
+              <Box mt={4}>
+                <Typography variant="h5" component="h2" gutterBottom>
+                  About Me
+                </Typography>
+                <Typography variant="body1" paragraph>
+                  I'm a passionate Front-End Developer with a knack for building
+                  user-friendly and efficient web applications.
+                </Typography>
 
-          <Typography variant="body1" paragraph>
-            我的开发哲学是“用户至上”，我相信优质的用户体验是每一个成功应用程序的核心。
-            因此，我致力于构建不仅美观且功能完善的界面，使得所有用户都能无障碍地访问。
-          </Typography>
-        </Box>
+                <Typography variant="body1" paragraph>
+                  My development philosophy is user-centric, aiming to find the
+                  perfect balance between visual design and user experience. I
+                  focus on maintainable and scalable code in projects, always
+                  eager to explore new technologies and continuously improve my
+                  skills.
+                </Typography>
+              </Box>
+            </Grid>
+            {/* 右侧部分 */}
+            {/* <Grid item xs={12} sm={5}>
+            <Box mt={5}>
+              <Typography variant="h5" component="h2" gutterBottom>
+                我的技能
+              </Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Typography variant="body1">HTML/CSS/JavaScript</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="body1">React/Material-UI</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="body1">无障碍性设计</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Typography variant="body1">Recoil/React Router</Typography>
+                </Grid>
+              </Grid>
+            </Box>
 
-        <Box mt={4}>
-          <Typography variant="h5" component="h2" gutterBottom>
-            我的技能
-          </Typography>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1">HTML/CSS/JavaScript</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1">React/Material-UI</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1">无障碍性设计</Typography>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Typography variant="body1">Recoil/React Router</Typography>
-            </Grid>
+            <Box mt={4}>
+              <Typography variant="h5" component="h2" gutterBottom>
+                联系我
+              </Typography>
+              <Typography variant="body1">如果你对我的工作感兴趣，欢迎通过以下方式与我联系：</Typography>
+              <Typography variant="body1">
+                Email: your.email@example.com
+              </Typography>
+              <Typography variant="body1">
+                LinkedIn: linkedin.com/in/your-profile
+              </Typography>
+            </Box>
+          </Grid> */}
           </Grid>
-        </Box>
-
-        <Box mt={4}>
-          <Typography variant="h5" component="h2" gutterBottom>
-            联系我
-          </Typography>
-          <Typography variant="body1">如果你对我的工作感兴趣，欢迎通过以下方式与我联系：</Typography>
-          <Typography variant="body1">Email: your.email@example.com</Typography>
-          <Typography variant="body1">
-            LinkedIn: linkedin.com/in/your-profile
-          </Typography>
-        </Box>
-      </Container>
+        </Container>
+      </Box>
     </Box>
   );
 };
